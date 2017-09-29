ef31c42eb4ee710915a1863a81ffb1354512abcb
lapadol <lapadol@msn.com>
2017-09-29 15:15:25 +0700

    startup 02

---
+++ XAMR/XAMR/XAMR.UWP/Views/AddItems.xaml.cs
@@ -0,0 +1,25 @@
+﻿using XAMR.Helpers;
+using XAMR.Model;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace XAMR.UWP.Views
+{
+    public sealed partial class AddItems : Page
+    {
+        public AddItems()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SaveItem_Click(object sender, RoutedEventArgs e)
+        {
+            var _item = new Item();
+            _item.Text = txtText.Text;
+            _item.Description = txtDesc.Text;
+            MessagingCenter.Send(this, "AddItem", _item);
+            MessagingCenter.ClearSubscribers();
+            this.Frame.GoBack();
+        }
+    }
+}
---
+++ XAMR/XAMR/XAMR.UWP/Views/BrowseItemDetail.xaml.cs
@@ -0,0 +1,23 @@
+﻿using XAMR.Model;
+using XAMR.ViewModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace XAMR.UWP.Views
+{
+    public sealed partial class BrowseItemDetail : Page
+    {
+        public ItemDetailViewModel ViewModel { get; set; }
+
+        public BrowseItemDetail()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ViewModel = new ItemDetailViewModel((Item)e.Parameter);
+            DataContext = ViewModel;
+        }
+    }
+}
